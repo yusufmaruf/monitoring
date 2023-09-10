c269b248972b8ffd78d2b0395c7072fc1ca706ba
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2023-09-10 22:04:35 +0700

    update foreign key

---
+++ database/migrations/2023_09_10_150301_add_foreign_tasks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreign('idSector')->references('idSector')->on('sectors')->onUpdate('cascade');
+            $table->foreign('idUser')->references('idUser')->on('users')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+};
